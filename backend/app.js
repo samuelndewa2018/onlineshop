18be10ef8a73aae264cdef97bcfa48b643b47ffd
Bramuels <samuelndewa2018@gmail.com>
2023-07-26 07:53:16 -0700

    cors

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,7 @@ const path = require("path");
 
 app.use(
   cors({
-    origin: "https://onlineshop-delta.vercel.app/",
+    origin: ["https://onlineshop-delta.vercel.app/"],
     // origin: ['https://eshop-tutorial-cefl.vercel.app','http://localhost:3000'],
     credentials: true,
   })
