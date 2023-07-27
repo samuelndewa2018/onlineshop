96d1c7125a50ffc32957ce85280c9f48898c4f56
Bramuels <samuelndewa2018@gmail.com>
2023-07-27 07:37:49 -0700

    cors security

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,8 @@ const cors = require("cors");
 app.use((req, res, next) => {
   res.setHeader(
     "Access-Control-Allow-Origin",
-    "https://onlineshop-2xjp.vercel.app"
+    "https://onlineshop-2xjp.vercel.app",
+    "https://onlineshop-delta.vercel.app/"
   );
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
@@ -18,7 +19,10 @@ app.use((req, res, next) => {
 
 app.use(
   cors({
-    origin: "https://onlineshop-delta.vercel.app/",
+    origin: [
+      "https://onlineshop-delta.vercel.app/",
+      "https://onlineshop-2xjp.vercel.app",
+    ],
     // origin: ['https://eshop-tutorial-cefl.vercel.app','http://localhost:3000'],
     credentials: true,
   })
