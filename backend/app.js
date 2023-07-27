7f69c9fd946e291480e8eff9f81141a340548bc6
Bramuels <samuelndewa2018@gmail.com>
2023-07-27 07:10:40 -0700

    cors security updates

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,8 @@ const cors = require("cors");
 app.use((req, res, next) => {
   res.setHeader(
     "Access-Control-Allow-Origin",
-    "https://onlineshop-2xjp.vercel.app"
+    "https://onlineshop-2xjp.vercel.app",
+    "https://onlineshop-delta.vercel.app/"
   );
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
