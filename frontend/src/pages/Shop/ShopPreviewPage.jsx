637d6225560458c82ba1c3ba8ce73666717b1572
Bramuels <samuelndewa2018@gmail.com>
2023-07-28 01:41:02 -0700

    shop products page

--- frontend/src/pages/Shop/ShopPreviewPage.jsx
+++ frontend/src/pages/Shop/ShopPreviewPage.jsx
@@ -17,7 +17,7 @@ const ShopPreviewPage = () => {
           <div className="800px:w-[25%] bg-[#fff] rounded-[4px] shadow-sm 800px:overflow-y-scroll 800px:h-[90vh] 800px:sticky top-16 left-0 z-10">
             <ShopInfo isOwner={false} />
           </div>
-          <div className="800px:w-[72%] mt-5 800px:mt-['unset'] rounded-[4px]">
+          <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-4 mb-12 lg:mb-12 border-0">
             <ShopProfileData isOwner={false} />
           </div>
         </div>
