9b9825729ecee4fcbb241d69eb1288995efa4692
Bramuels <samuelndewa2018@gmail.com>
2023-08-05 12:53:18 -0700

    location trial

--- frontend/src/Trial/TrialPage.js
+++ frontend/src/Trial/TrialPage.js
@@ -1,43 +1,81 @@
 import React, { useState } from "react";
+import * as yup from "yup";
+import { useFormik } from "formik";
+import axios from "axios";
 
-const LocationPicker = () => {
-  const [latitude, setLatitude] = useState(null);
-  const [longitude, setLongitude] = useState(null);
-  const [errorMessage, setErrorMessage] = useState(null);
+const schema = yup.object().shape({
+  address: yup.string().required("Address is required"),
+});
 
-  const getLocation = () => {
+function App() {
+  const [currentLocation, setCurrentLocation] = useState("");
+  const formik = useFormik({
+    initialValues: {
+      address: "",
+    },
+    validationSchema: schema,
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
+
+  const handleGetCurrentLocation = () => {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
         (position) => {
-          setLatitude(position.coords.latitude);
-          setLongitude(position.coords.longitude);
-          setErrorMessage(null);
+          const { latitude, longitude } = position.coords;
+          setCurrentLocation("Fetching location...");
+          const url = `https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${latitude}&lon=${longitude}`;
+          axios
+            .get(url)
+            .then((response) => {
+              setCurrentLocation(response.data.display_name);
+              formik.setFieldValue("address", response.data.display_name);
+            })
+            .catch((error) => {
+              console.error("Error fetching location:", error);
+              setCurrentLocation("Location fetch failed.");
+            });
         },
         (error) => {
-          setErrorMessage(
-            "Error getting your location. Please allow location access."
-          );
+          console.error("Error getting current location:", error);
         }
       );
     } else {
-      setErrorMessage("Geolocation is not supported by your browser.");
+      console.error("Geolocation is not supported by this browser.");
     }
   };
 
   return (
     <div>
-      {errorMessage && <p>{errorMessage}</p>}
-      {latitude && longitude ? (
+      <form onSubmit={formik.handleSubmit}>
         <div>
-          <p>Latitude: {latitude}</p>
-          <p>Longitude: {longitude}</p>
-          {/* Use this data to display pickup stations or perform other actions */}
+          <label htmlFor="address">Address</label>
+          <input
+            type="text"
+            name="address"
+            value={formik.values.address}
+            onChange={formik.handleChange}
+          />
+          {formik.errors.address && formik.touched.address && (
+            <p>{formik.errors.address}</p>
+          )}
         </div>
-      ) : (
-        <button onClick={getLocation}>Get My Location</button>
+        <button type="button" onClick={handleGetCurrentLocation}>
+          Use Current Location
+        </button>
+        <button type="submit">Submit</button>
+      </form>
+      {currentLocation && (
+        <>
+          <p>Current Location:</p>
+          {currentLocation.split(",").map((part, index) => (
+            <p key={index}>{part.trim()},</p>
+          ))}
+        </>
       )}
     </div>
   );
-};
+}
 
-export default LocationPicker;
+export default App;
