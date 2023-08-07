e07fb88832a6e076d78aa6077d6a702a9c4131f4
Bramuels <samuelndewa2018@gmail.com>
2023-08-07 02:27:56 -0700

    terms and conditions

--- frontend/src/components/Signup/Signup.jsx
+++ frontend/src/components/Signup/Signup.jsx
@@ -261,10 +261,8 @@ const Singup = () => {
                     className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
                   />
                   <label
-                    type="checkbox"
-                    name="remember-me"
-                    id="remember-me"
-                    className="h-4 w-4 text-blue-600 focus:ring-blue-500 border-gray-300 rounded"
+                    for="link-checkbox"
+                    className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
                   >
                     I agree with the{" "}
                     <Link
