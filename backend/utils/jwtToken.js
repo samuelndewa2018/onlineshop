c95177716ba9de77274f7d8e319aa383bbe8ff8e
Bramuels <samuelndewa2018@gmail.com>
2023-08-08 22:26:28 -0700

    SECURITY REMOVED

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -6,15 +6,15 @@ const sendToken = (user, statusCode, req, res) => {
   const options = {
     expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
     httpOnly: true,
-    sameSite: "none", //when backend and frontend are not on the same site
-    secure: true,
+    // sameSite: "none", //when backend and frontend are not on the same site
+    // secure: true,
   };
 
   // Check if the user agent is Safari and adjust the sameSite option
-  const userAgent = req.get("User-Agent");
-  if (userAgent.includes("Safari") && !userAgent.includes("Chrome")) {
-    options.sameSite = "lax"; // Use "lax" for Safari
-  }
+  // const userAgent = req.get("User-Agent");
+  // if (userAgent.includes("Safari") && !userAgent.includes("Chrome")) {
+  //   options.sameSite = "lax"; // Use "lax" for Safari
+  // }
 
   res.status(statusCode).cookie("token", token, options).json({
     success: true,
