6530c6f2f2437d1cabfd3c86964aa69401f3e5e4
Bramuels <samuelndewa2018@gmail.com>
2023-09-21 04:19:15 -0700

    pg w-full

--- frontend/src/components/Events/EventCard.jsx
+++ frontend/src/components/Events/EventCard.jsx
@@ -26,7 +26,7 @@ const EventCard = ({ active, data }) => {
     }
   };
   return (
-    <div class="container">
+    <div class="container w-full">
       <div class="block rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
         <div class="flex flex-wrap items-center">
           <div class="block w-full shrink-0 grow-0 basis-auto lg:flex lg:w-6/12 xl:w-4/12">
