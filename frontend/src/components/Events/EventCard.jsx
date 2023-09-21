ee45edc6f4840b40f8a04c34e1920a548ca4f44a
Bramuels <samuelndewa2018@gmail.com>
2023-09-21 03:55:34 -0700

    epage px

--- frontend/src/components/Events/EventCard.jsx
+++ frontend/src/components/Events/EventCard.jsx
@@ -27,7 +27,7 @@ const EventCard = ({ active, data }) => {
   };
   return (
     <>
-      <div class="container my-24 mx-auto md:px-6 xl:px-32">
+      <div class="container my-24">
         <section class="mb-32">
           <div class="block rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
             <div class="flex flex-wrap items-center">
