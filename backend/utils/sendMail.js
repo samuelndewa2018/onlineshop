cb205183a585364658d7b191cfa9b6a3dc121ae7
Bramuels <samuelndewa2018@gmail.com>
2023-10-05 13:12:23 -0700

    smpt service

--- backend/utils/sendMail.js
+++ backend/utils/sendMail.js
@@ -4,7 +4,7 @@ const sendMail = async (options) => {
   const transporter = nodemailer.createTransport({
     host: process.env.SMPT_HOST,
     port: process.env.SMPT_PORT,
-    service: process.env.SMPT_SERVICE,
+    // service: process.env.SMPT_SERVICE,
     auth: {
       user: process.env.SMPT_MAIL,
       pass: process.env.SMPT_PASSWORD,
