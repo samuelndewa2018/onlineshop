8e07099c1d32e4e28e4c4ffaca07345b7b21ae4e
Corne Craig <cornecraig26@gmail.com>
2024-01-30 17:18:54 +0300

    kl

--- backend/utils/baseUrl.js
+++ backend/utils/baseUrl.js
@@ -3,5 +3,5 @@ export const baseUrl = () =>
     ? process.env.BASE_URL
     : process.env.NODE_ENV !== "production"
     ? "http://localhost:3000"
-    : "https://yourdomain.com";
+    : "https://www.ninetyone.co.ke";
 //for purpose of upload
